8e51da371db99189c0eb2156edd7f49f283c51ae
chen-weixin <chensprite@gmail.com>
2026-06-12 22:11:48 +0800

    feat: add PWA app skeleton

---
+++ pwa/js/app.js
@@ -0,0 +1,7 @@
+console.info("Aluminum profile designer PWA loaded");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
